de930cb6ebb7010accd983cb9afaacb0e57751e0
Exxon <tomfagan03@gmail.com>
2021-04-01 23:18:31 +0100

    Fixed github pages issue where it wasn't playing sounds

--- js/index.js
+++ js/index.js
@@ -19,7 +19,7 @@ function playSound(number) {
 
     switch (number) {
         case '1':
-            audio = new Audio('../sounds/1-rick.mp3');
+            audio = new Audio('sounds/1-rick.mp3');
             return audio.play();
             break;
         case '2':
@@ -27,31 +27,31 @@ function playSound(number) {
             return audio.play();
             break;
         case '3':
-            audio = new Audio('3-coffin.mp3');
+            audio = new Audio('sounds/3-coffin.mp3');
             return audio.play();
             break;
         case '4':
-            audio = new Audio('../sounds/4-doIt.mp3');
+            audio = new Audio('sounds/4-doIt.mp3');
             return audio.play();
             break;
         case '5':
-            audio = new Audio('../sounds/5-monitor.mp3');
+            audio = new Audio('/sounds/5-monitor.mp3');
             return audio.play();
             break;
         case '6':
-            audio = new Audio('../sounds/6-shotsFired.mp3');
+            audio = new Audio('sounds/6-shotsFired.mp3');
             return audio.play();
             break;
         case '7':
-            audio = new Audio('../sounds/7-cares.mp3');
+            audio = new Audio('sounds/7-cares.mp3');
             return audio.play();
             break;
         case '8':
-            audio = new Audio('../sounds/8-bruh.mp3');
+            audio = new Audio('sounds/8-bruh.mp3');
             return audio.play();
             break;
         case '9':
-            audio = new Audio('../sounds/9-again.mp3');
+            audio = new Audio('sounds/9-again.mp3');
             return audio.play();
             break;
     }
