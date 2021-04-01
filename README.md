d2395d5816aad69fca20c4e8ab762eb7426357fd
Exxon <tomfagan03@gmail.com>
2021-04-01 14:31:48 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# A very bad soundboard
+
+I wouldn't recommend this soundboard, for a start you can only have 9 sounds with the way it does it, it has basically no css because I hate css and the js in it is probs the long way of doing it. :)
---
+++ js/index.js
@@ -0,0 +1,56 @@
+let audio;
+
+
+document.querySelectorAll('.sound').forEach(button => {
+    button.addEventListener('click', function () {
+        if (audio)
+            audio.pause()
+        playSound(button.innerHTML[0]);
+    });
+});
+
+function playSound(number) {
+
+    switch (number) {
+        case '1':
+            audio = new Audio('../sounds/1-rick.mp3');
+            return audio.play();
+            break;
+        case '2':
+            audio = new Audio('../sounds/2-gotHim.mp3');
+            return audio.play();
+            break;
+        case '3':
+            audio = new Audio('../sounds/3-coffin.mp3');
+            return audio.play();
+            break;
+        case '4':
+            audio = new Audio('../sounds/4-doIt.mp3');
+            return audio.play();
+            break;
+        case '5':
+            audio = new Audio('../sounds/5-monitor.mp3');
+            return audio.play();
+            break;
+        case '6':
+            audio = new Audio('../sounds/6-shotsFired.mp3');
+            return audio.play();
+            break;
+        case '7':
+            audio = new Audio('../sounds/7-cares.mp3');
+            return audio.play();
+            break;
+        case '8':
+            audio = new Audio('../sounds/8-bruh.mp3');
+            return audio.play();
+            break;
+        case '9':
+            audio = new Audio('../sounds/9-again.mp3');
+            return audio.play();
+            break;
+        default:
+            audio = new Audio('../sounds/1-rick.mp3');
+            return audio.play();
+            break;
+    }
+};
